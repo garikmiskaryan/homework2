b6bd7f82a15e7b88a7890db47a35fd5e5572a3bc
Garik <garikmiskaryan>
2017-09-12 17:39:03 +0400

    homework2

---
+++ homework2.js
@@ -0,0 +1,11 @@
+const a = "Sport";
+
+console.log(a);
+
+const g = "1000";
+console.log (g + 500);
+
+console.log((17 + 7) * 10);
+  console.log(8/4);
+    console.log(30+60-30);
+    	console.log(isNaN('true+ true'));
